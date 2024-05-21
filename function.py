b9817076ecd51e82d80a4c36fd9b1d664bebcdc8
Uwabor Emmanuel <uwabore@gmail.com>
2024-05-21 12:41:04 +0100

    updated

---
+++ function.py
@@ -0,0 +1,14 @@
+
+# # def checknumber(x):
+# #     if x % 2 == 0 and x < 10:
+# #         print(' the number is even and less than 10')
+# #     else:
+# #         print('the number is odd')
+
+
+# checknumber(9)
+def checksum(a,b):
+    sum = a + b
+    print(sum)
+
+checksum(5,5)
