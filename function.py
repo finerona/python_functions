6db4b7287a5d9f3742acac72d5af457639dbaac1
Uwabor Emmanuel <uwabore@gmail.com>
2024-05-27 19:35:46 +0100

    update on functions

--- function.py
+++ function.py
@@ -1,22 +1,22 @@
 
-# # def checknumber(x):
-# #     if x % 2 == 0 and x < 10:
-# #         print(' the number is even and less than 10')
-# #     else:
-# #         print('the number is odd')
+def checknumber(x):
+    if x % 2 == 0 and x < 10:
+        print(' the number is even and less than 10')
+    else:
+        print('the number is odd')
 
 
-# checknumber(9)
-# def checksum(a,b):
-#     sum = a + b
-#     print(sum)
+checknumber(9)
+def checksum(a,b):
+    sum = a + b
+    print(sum)
 
-# checksum(5,5)
-# def addnumbers(a,b):
-#     sum = a + b
-#     print(sum)
+checksum(5,5)
+def addnumbers(a,b):
+    sum = a + b
+    print(sum)
 
-# addnumbers(20,20)
+addnumbers(20,20)
 
 def checkage(dateofbirth,currentyear):
     age = dateofbirth - currentyear
@@ -24,10 +24,35 @@ def checkage(dateofbirth,currentyear):
 
 checkage(2024,1997)
 
+def stateandcapital(state):
+    states = {
+        "ikeja" : "Lagos",
+        "Edo" : "Benin",
+        "Abuja" : "FCT" 
 
+    }
+    for keys, values in states.items():
+        if keys == state:
+            capital = values
+            print(f'the capital of {state} is {capital}')
+stateandcapital('Edo')
 
+def multiplication(x,y):
+    mul = x * y
+    print(mul)
 
+multiplication(5,5)
 
+def Division(r,t):
+    div = r / t
+    print(div)
+Division(10,2)
 
-
-
+a = 10
+b = 20
+if a > b:
+    print('a is greater than be')  
+elif a == b:
+     print('a equals to b')
+else:
+    print('b is greater than a')
